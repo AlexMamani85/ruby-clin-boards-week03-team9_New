1f808ebb3d00f9e90ae19c8e4bfe23888b6f9f25
VientoGris <martin.v.ubillus@gmail.com>
2022-03-03 20:22:00 -0500

    menu

--- clin_boards.rb
+++ clin_boards.rb
@@ -1,3 +1,5 @@
+require "json"
+
 class ClinBoards
   def initialize
     # Complete this
@@ -8,6 +10,24 @@ class ClinBoards
   end
 end
 
+def banner
+  print "  ####################################
+  #      Welcome to CLIn Boards      #
+  ####################################\n"
+end
+
+def menu
+  print  "Board options: create | show ID | update ID | delete ID\n exit\n"
+  print "> "
+  action = gets.chomp
+end
+
+store = JSON.parse(File.read("store.json"), { symbolize_names: true })
+
 # get the command-line arguments if neccesary
-app = ClinBoards.new
-app.start
+# app = ClinBoards.new
+# app.start
+
+banner
+menu
+p store
